a955ec24ce5772506fcb763883e95bdfc83c5e4d
imaxzbotz <imeshnewhotmil@hotmail.com>
2024-09-21 10:15:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,21 +16,21 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownername: process.env.Owner_Name || "`@Iᴍᴀ_Xᴢ`",
+    ownernumber: process.env.Owner_Number || "94784924900",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
      * other details and parameters
      */
-    author: process.env.Author || "𝐏𝐢𝐤𝐚𝐁𝐨𝐭𝐳 🌚🌙",
+    author: process.env.Author || "@`IMA_Xzz`",
     packname: process.env.PackName || "𝕼𝖚𝖊𝖊𝖓 𝕬𝖓𝖞𝖆 𝖁2 𝕾𝖙𝖎𝖈𝖐𝖊𝖗 𝕻𝖆𝖈𝖐 🪀",
     socialLink: process.env.Web || "https://github.com/PikaBotz",
     groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://imeshnewhotmail:2WTp4IQxmGDniqJX@cluster0.uhscl.mongodb.net/?retryWrites=true&w=majority",
+    sessionId: process.env.SESSION_ID || "YXcyODI_Queen-Anya_1eHY=",
 
     /**
      * thumbnails and medias
